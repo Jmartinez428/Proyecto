193d70fcf02cf2e2ca5688078801e619ad2eede5
Sebastian Martinez <jmartinez428@unab.edu.co>
2019-03-25 18:48:46 -0500

    Lectura y envio de notas a python

---
+++ app/database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class BaseDatos():
+
+def __init__(self):
+    pass
+
+
+def crearTabla(self):
+    conn = sqlite3.connect('data.notasdb')
+    c = conn.cursor()
+    c.execute('CREATE TABLE Notas (materia text, porcentaje real, nota real')
+    conn.commit()
+    conn.close()
+
+def insertar(req, porcentaje, nota):
+    conn = sqlite3.connect('data.notasdb')
+    c = conn.cursor()
+    c.execute("INSERT INTO Notas VALUES ('materia', porcentaje, nota)")
+    conn.commit()
+    conn.close()
+
+def leer(self):
+    conn = sqlite3.connect('data.notasdb')
+    c = conn.cursor()
+    c.execute("SELECT porcentaje, nota FROM Notas")
+    u = c.fetchone()
+    conn.close()
+    return u
+
+
+
--- app/server.py
+++ app/server.py
@@ -2,6 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/notaMateria', methods=['GET'])
+def formulario():
+    
+    return render_template('inicio.html')   
+
+@app.route('/notaMateria', methods=['POST'])
+def notaMateria():
+    # db = BaseDatos()
+    # db.crearTabla()
+    # definitiva = db.leer()
+    # return definitiva
+    nota = request.form['nota']
+    porcentaje = request.form['porcentaje']
+    defin = (porcentaje/100)*nota
+    return defin
+
 
 @app.route('/inicio')
 def metodo():
